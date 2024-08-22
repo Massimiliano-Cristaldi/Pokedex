5910569b314beded9ee9c412684e5b0599872a2d
Massimiliano Cristaldi <cristaldi.massimiliano@gmail.com>
2024-08-22 17:28:39 +0200

    made a minor fix to the way HTTP request error messages are displayed in the error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -9,7 +9,7 @@ export default function ErrorPage(){
     return(
         <div className="errorPage">
             <h1>Oops! Something went wrong</h1>
-            <h3 className="my-4">{`${error?.status} - ${error?.statusText}`}</h3>
+            <h3 className="my-4">{`${error.status || "??"} - ${error.statusText || "Unidentified error"}`}</h3>
             <h3>{error?.message}</h3>
             <button className="mb-2" onClick={()=>{navigate(-1)}}>Back</button>
         </div>
